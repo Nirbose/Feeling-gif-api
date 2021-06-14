2f1a3b87067e25a7d7a2680302a00cedaad7ae10
Nirbose <nirbose2@gmail.com>
2021-06-14 13:10:00 +0200

    Update

--- Api/Api.php
+++ Api/Api.php
@@ -10,40 +10,127 @@ class Api {
 
     public function __construct()
     {
-        $this->path = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'];
+        $this->path = "https://enzia.toile-libre.org/cdn/feeling/";
     }
 
-    public function show($test) {
-        echo $test;
+    public function id(int $id = null) {
+        if(is_null($id)) {
+            return rand($this->rmin, $this->rmax);
+        } else {
+            return $id;
+        }
     }
 
-    public function hug() {
-        header('Content-Type: application/json');
+    public function check(string $feeling, int $id) {
+        $url = "https://enzia.toile-libre.org/cdn/feeling/script.php";
+        $rep = json_decode(file_get_contents($url . "?feeling=".$feeling."&id=".$id));
+        if($rep->success == 200) {
+            return $rep->exist;
+        } else {
+            return false;
+        }        
 
-        $random = rand($this->rmin, $this->rmax);
-
-        $json = [];
-        $json['success'] = 200;
-        $json['feeling'] = "Hug";
-        $json['link'] = $this->path . "/imgs/hug/" . $random . ".gif";
-
-        $this->encode($json);
     }
 
-    public function kiss() {
-        header('Content-Type: application/json');
+    public function hug(int $id = null) {
 
-        $random = rand($this->rmin, $this->rmax);
+        $imageID = $this->id($id);
+        if($this->check("hug", $imageID)) {
 
-        $json = [];
-        $json['success'] = 200;
-        $json['feeling'] = "kiss";
-        $json['link'] = $this->path . "/imgs/kiss/" . $random . ".gif";
+            header('Content-Type: application/json');
 
-        $this->encode($json);
+            $json = [];
+            $json['success'] = 200;
+            $json['feeling'] = "Hug";
+            $json['link'] = $this->path . "hug/" . $imageID . ".gif";
+
+            $this->show($json);
+
+        } else {
+            $this->hug(rand($this->rmin, $this->rmax));
+        }
     }
 
-    public function encode(array $json) {
+    public function kiss(int $id = null) {
+
+        $imageID = $this->id($id);
+
+        if($this->check("kiss", $imageID)) {
+
+            header('Content-Type: application/json');
+
+            $json = [];
+            $json['success'] = 200;
+            $json['feeling'] = "kiss";
+            $json['link'] = $this->path . "kiss/" . $imageID . ".gif";
+
+            $this->show($json);
+        } else {
+            $this->kiss(rand($this->rmin, $this->rmax));
+        }
+    }
+
+    public function pat(int $id = null)
+    {
+        $imageID = $this->id($id);
+
+        if($this->check("pat", $imageID)) {
+
+            header('Content-Type: application/json');
+
+            $json = [];
+            $json['success'] = 200;
+            $json['feeling'] = "kiss";
+            $json['link'] = $this->path . "pat/" . $imageID . ".gif";
+
+            $this->show($json);
+        } else {
+            $this->pat(rand($this->min, $this->max));
+        }
+    }
+
+    public function cry(int $id = null)
+    {
+        $imageID = $this->id($id);
+
+        if($this->check("cry", $imageID)) {
+
+            header('Content-Type: application/json');
+
+            $json = [];
+            $json['success'] = 200;
+            $json['feeling'] = "kiss";
+            $json['link'] = $this->path . "cry/" . $imageID . ".gif";
+
+            $this->show($json);
+
+        } else {
+            $this->cry(rand($this->min, $this->max));
+        }
+
+    }
+
+
+    public function eat(int $id = null)
+    {
+        $imageID = $this->id($id);
+
+        if($this->check("eat", $imageID)) {
+
+            header('Content-Type: application/json');
+
+            $json = [];
+            $json['success'] = 200;
+            $json['feeling'] = "kiss";
+            $json['link'] = $this->path . "eat/" . $imageID . ".gif";
+
+            $this->show($json);
+        } else {
+            $this->eat(rand($this->min, $this->max));
+        }
+    }
+
+    public function show(array $json) {
         echo json_encode($json);
     }
 
--- index.php
+++ index.php
@@ -5,10 +5,35 @@ require __DIR__ . '/vendor/autoload.php';
 $router = new \Bramus\Router\Router();
 
 $router->get('/', function() {
-    var_dump($_SERVER);
+    var_dump(file_get_contents("https://enzia.toile-libre.org/cdn/feeling/script.php"));
 });
 
+$router->get('/random', "Api\Api@random");
+
 $router->get('/hug', "Api\Api@hug");
+$router->get('/hug/(\d+)', "Api\Api@hug");
+
 $router->get('/kiss', "Api\Api@kiss");
+$router->get('/kiss/(\d+)', "Api\Api@kiss");
+
+$router->get('/pat', "Api\Api@pat");
+$router->get('/pat/(\d+)', "Api\Api@pat");
+
+$router->get('/cry', "Api\Api@cry");
+$router->get('/cry/(\d+)', "Api\Api@cry");
+
+$router->get('/eat', "Api\Api@eat");
+$router->get('/eat/(\d+)', "Api\Api@eat");
+
+
+$router->set404(function() {
+    header('HTTP/1.1 404 Not Found');
+    header('Content-Type: application/json');
+
+    $json['success'] = 404;
+    $json['err'] = "The file was not found";
+
+    echo json_encode($json);
+});
 
 $router->run();
